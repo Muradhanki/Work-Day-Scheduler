6c4cdf2835dc1faa5ca8002af1d6732c2980c938
Muradhanki <muradhankilinc@gmail.com>
2024-01-21 18:14:02 +0000

    JS file Instructions engine

--- script.js
+++ script.js
@@ -1,57 +1,109 @@
- // Instructs engine to load the HTML and CSS before running the JS
- $(document).ready(function () {
-    const dayjs = require('dayjs')
-//import dayjs from 'dayjs' // ES 2015
-dayjs().format()
-// Create a variable and use query selector to display current date and time
-var displayTime = document.querySelector("#currentDay");
+//  // Instructs engine to load the HTML and CSS before running the JS
+//  $(document).ready(function () {
+//     const dayjs = require('dayjs')
 
-// Use dayjs to display current date and time in given format
-var currentTime = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
+// // Create a variable and use query selector to display current date and time
+// var displayTime = document.querySelector("#currentDay");
 
-displayTime.textContent = currentTime;
+// // Use dayjs to display current date and time in given format
+// var currentTime = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
 
- // Assign saveBtn click listener for user input and get row id and save to local storage
- $(".saveBtn").on("click", function () {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+// displayTime.textContent = currentTime;
 
-    // Save text in local storage
-    localStorage.setItem(time, text);
+//  // Assign saveBtn click listener for user input and get row id and save to local storage
+//  $(".saveBtn").on("click", function () {
+//     var text = $(this).siblings(".description").val();
+//     var time = $(this).parent().attr("id");
+
+//     // Save text in local storage
+//     localStorage.setItem(time, text);
+// });
+
+// function hourTracker() {
+//     // Get current number of hours.
+//     var currentHour = dayjs().hour();
+  
+//     // Loop over time blocks
+//     $(".time-block").each(function () {
+//       var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+//        // Check the time and add the classes for background indicators
+//        if (blockHour < currentHour) {
+//         $(this).addClass("past");
+//       } else if (blockHour === currentHour) {
+//         $(this).removeClass("past");
+//         $(this).addClass("present");
+//       } else {
+//         $(this).removeClass("past");
+//         $(this).removeClass("present");
+//         $(this).addClass("future");
+//       }
+//     });
+//   }
+//   hourTracker();
+
+//   // Create a function to loop over time blocks to retrieve and display data from local storage
+// function displayText() {
+//     // Loop over time blocks
+//     $(".time-block").each(function () {
+//       // var blockHour = parseInt($(this).attr("id").split("-")[1]);
+//       var blockHour = $(this).attr("id");
+//       $(this).children(".description").val(localStorage.getItem(blockHour));
+//     });
+//   }
+//   displayText();
+  
+//   });
+
+// Instructs engine to load the HTML and CSS before running the JS
+$(document).ready(function () {
+  
+    const dayjs = window.dayjs;
+
+    // Create a variable and use query selector to display current date and time
+    var displayTime = $("#currentDay"); // Use jQuery selector directly
+
+    // dayjs to display current date and time in the given format
+    var currentTime = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
+
+    displayTime.text(currentTime); 
+
+    // saveBtn click listener for user input and get row id and save to local storage
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // text saving in local storage
+        localStorage.setItem(time, text);
+    });
+
+    function hourTracker() {
+        // Current number of hours using hour() method from dayjs
+        var currentHour = dayjs().hour();
+
+        // Loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+            // The time checker and the classes adder for background indicators
+            if (blockHour < currentHour) {
+                $(this).addClass("past").removeClass("present future");
+            } else if (blockHour === currentHour) {
+                $(this).removeClass("past future").addClass("present");
+            } else {
+                $(this).removeClass("past present").addClass("future");
+            }
+        });
+    }
+    hourTracker();
+
+    // Function creator to loop over time blocks to retrieve and display data from local storage
+    function displayText() {
+        // Loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = $(this).attr("id");
+            $(this).children(".description").val(localStorage.getItem(blockHour));
+        });
+    }
+    displayText();
 });
-
-function hourTracker() {
-    // Get current number of hours.
-    var currentHour = dayjs().hour();
-  
-    // Loop over time blocks
-    $(".time-block").each(function () {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-
-       // Check the time and add the classes for background indicators
-       if (blockHour < currentHour) {
-        $(this).addClass("past");
-      } else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-      } else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
-    });
-  }
-  hourTracker();
-
-  // Create a function to loop over time blocks to retrieve and display data from local storage
-function displayText() {
-    // Loop over time blocks
-    $(".time-block").each(function () {
-      // var blockHour = parseInt($(this).attr("id").split("-")[1]);
-      var blockHour = $(this).attr("id");
-      $(this).children(".description").val(localStorage.getItem(blockHour));
-    });
-  }
-  displayText();
-  
-  });
