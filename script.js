90409a91e13cecc53fc12a51e974176829e6af80
Muradhanki <muradhankilinc@gmail.com>
2024-01-21 00:41:24 +0000

    JS file Instructs engine to load the HTML and CSS before running the JS

---
+++ script.js
@@ -0,0 +1,6 @@
+ // Instructs engine to load the HTML and CSS before running the JS
+ $(document).ready(function () {
+    const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+});
