57146b915a57572624e3d66183fbe4c4549be865
Muradhanki <muradhankilinc@gmail.com>
2024-01-21 00:42:15 +0000

    JS file Use dayjs to display current date and time in given format

--- script.js
+++ script.js
@@ -3,4 +3,11 @@
     const dayjs = require('dayjs')
 //import dayjs from 'dayjs' // ES 2015
 dayjs().format()
+// Create a variable and use query selector to display current date and time
+var displayTime = document.querySelector("#currentDay");
+
+// Use dayjs to display current date and time in given format
+var currentTime = dayjs().format("dddd, MMMM D, YYYY, h:mm:ss a");
+
+displayTime.textContent = currentTime;
 });
